5817daad2d5fae3b41f3a733fb14bf528e15e032
Eugene Sherlaimov <sherlaimov@gmail.com>
2016-03-31 00:33:52 +0300

    major UI elms to directives

--- app/main/MainCtrl.js
+++ app/main/MainCtrl.js
@@ -45,23 +45,11 @@ function MainCtrl($scope, pokeApi) {
                     total_count: result.data.meta.total_count
                 };
                 _paginationInit();
-                console.log(main.meta);
 
             })
             .catch(_showError);
     }
     _updateModel();
-
-    function _findNext(){
-        _updateModel(main.meta.next);
-    }
-    main.findNext = _findNext;
-
-    function _findPrev(){
-        if ( main.meta.previous !== null)
-        _updateModel(main.meta.previous);
-    }
-    main.findPrev = _findPrev;
 
     function _findById(id){
         console.log(id);
@@ -79,30 +67,79 @@ function MainCtrl($scope, pokeApi) {
 }
 
 angular.module('PokeDex')
-    .directive('pagination', function pagination(){
+    .directive('infoBox', function infoBox(){
         return {
             scope: {
-                meta: '='
+                pokemon: '=',
+                types: '='
             },
             replace: true,
             template: [
-                '<nav><ul class="pagination">',
-                        '<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>',
-                        '<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>',
-                        '<li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>',
-                '</ul></nav>'
+            '<div class="info-box">',
+            '<ul class="list-group">',
+            '<img src="http://pokeapi.co/media/img/{{pokemon.national_id}}.png " alt="{{mainCtrl.pokemon.name}}">',
+            '<h4>{{mainCtrl.pokemon.name}}</h4>',
+            '<li class="list-group-item"> Attack <span class="badge">{{pokemon.attack}}</span></li>',
+            '<li class="list-group-item">Type <span class="badge" ng-repeat="type in types">{{type.name}}</span></li>',
+            '<li class="list-group-item">Defense <span class="badge">{{pokemon.defense}}</span></li>',
+            '<li class="list-group-item">HP <span class="badge">{{pokemon.hp}}</span></li>',
+            '<li class="list-group-item">SP Attack <span class="badge">{{pokemon.sp_atk}}</span></li>',
+            '<li class="list-group-item">SP Defense <span class="badge">{{pokemon.sp_def}}</span></li>',
+            '<li class="list-group-item">Speed <span class="badge">{{pokemon.speed}}</span></li>',
+            '<li class="list-group-item"> Weight <span class="badge">{{pokemon.weight}}</span></li>',
+            '<li class="list-group-item">Total moves <span class="badge">{{pokemon.moves.length}}</span></li>',
+            '</ul></div>'
+            ].join(''),
+            link: function(scope, element, attrs) {
+                $(window).scroll(function(e){
+                    //console.log(e);
+                    var $el = $('.info-box');
+                    var isPositionFixed = ($el.css('position') == 'fixed');
+                    if ($(this).scrollTop() > 116 && !isPositionFixed){
+                        $el.addClass('info-box-sticky');
+                    }
+                    if ($(this).scrollTop() < 116 && isPositionFixed)
+                    {
+                        $el.removeClass('info-box-sticky');
+                    }
+
+                });
+
+            }
+        };
+    });
+
+angular.module('PokeDex')
+    .directive('pokeBox', function pokeBox(){
+        return {
+            scope: {
+                pokemons: '='
+            },
+            replace: true,
+            template: [
+            '<div class="col-sm-6 col-md-4" ng-repeat="pokemon in pokemons">',
+            '<a href="javascript:void(0)" ng-click="mainCtrl.findById(pokemon.id)" uib-popover="Click for more info" popover-trigger="mouseenter">',
+            '<div class="thumbnail poke-box">',
+            '<div class="poke-img">',
+            '<img src="http://pokeapi.co/media/img/{{pokemon.id}}.png " alt="{{pokemon.name}}"></div>',
+            '<div class="caption"><h3>{{pokemon.name}}</h3>',
+            '<ul class="nav nav-pills" role="tablist">',
+            '<li ng-repeat="type in pokemon.types"><span class="badge poke-badge">{{type.name}}</span></li>',
+            '</ul></div></div></a></div>'
 
             ].join(''),
             link: function(scope, element, attrs) {
-                var meta = null;
-                console.log(scope.meta);
-                scope.$watch('meta', function(value){
-                    meta =  value;
-                    console.log(meta);
-                    if ( ! jQuery.isEmptyObject(meta)) {
-                        console.log('WORKS');
-                    }
-                });
+                //реализовать тултип на div element
+
+                var options = {
+                    placement: 'top',
+                    html: true,
+                    template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
+                };
+                var test = element.find('[data-toggle="tooltip"]').tooltip(options);
+                console.log(element.children(1).tooltip(options));
+                console.log(test);
+                //console.log(angular.element(element[0]));
 
 
             }
--- vendor/helper.js
+++ vendor/helper.js
@@ -1,19 +1,22 @@
-$(window).scroll(function(e){
-    //console.log(e);
-    var $el = $('.info-box');
-    var isPositionFixed = ($el.css('position') == 'fixed');
-    if ($(this).scrollTop() > 116 && !isPositionFixed){
-        $el.addClass('info-box-sticky');
-    }
-    if ($(this).scrollTop() < 116 && isPositionFixed)
-    {
-        $el.removeClass('info-box-sticky');
-    }
-    var options = {
-        placement: 'top',
-        html: true,
-        template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
-    };
-    $('[data-toggle="tooltip"]').tooltip(options);
+//$(window).scroll(function(e){
+//    //console.log(e);
+//    var $el = $('.info-box');
+//    var isPositionFixed = ($el.css('position') == 'fixed');
+//    if ($(this).scrollTop() > 116 && !isPositionFixed){
+//        $el.addClass('info-box-sticky');
+//    }
+//    if ($(this).scrollTop() < 116 && isPositionFixed)
+//    {
+//        $el.removeClass('info-box-sticky');
+//    }
+//
+//});
 
+$(function(){
+    //var options = {
+    //    placement: 'top',
+    //    html: true,
+    //    template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
+    //};
+    //$('[data-toggle="tooltip"]').tooltip(options);
 });
